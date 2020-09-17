c54dc1b9b089416b5c13c18822aa2afa8abddcae
qfc5036 <qfc5036@psu.edu>
2020-09-17 18:46:18 +0000

    k

--- main.c
+++ main.c
@@ -3,27 +3,28 @@
 // Collaborator: Shufang Huang sbh5655@psu.edu
 // Collaborator: Isabel Vera ikv5018@psu.edu
 #include <stdio.h>
-#include <string.h>
 #include <readline/readline.h>
+#include <stdlib.h>
+
 int sum_n(int n){
   if (n == 0){
     return 0;
   }
-  else{
-    return n + sum_n(n-1);
-  }
+    else{
+      return n + sum_n(n-1);
+    }
 }
 void print_n(const char *s, int n){
-  if (n !=0){
+  if (n != 0){
     printf("%s\n",s);
-    print_n(s,n-1);
+    print_n(s, n-1);
   }
 }
 
 int main(void){
-  int n1 = atoi(readline("Enter an int: "));
-  printf("sum is %d.\n", sum_n(n1));
+  int F = atoi(readline("Enter an int: "));
+  printf("sum is %d.\n",sum_n(F));
   char *string = readline("Enter a string: ");
-  print_n(string,n1);
+  print_n(string,F);
   return 0;
 }
